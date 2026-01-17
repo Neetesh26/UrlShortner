dfc7a6a4f90b2d4c6cf4154188559047bfb82e6f
Neetesh26 <neeteshprajapati009@gmail.com>
2026-01-17 12:05:17 +0530

    fix: update MongoDB connection string to use MONGODB_CONNECTION_ATLAS

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const dbConnection = async (req, res) => {
     try {
-        const connection = mongoose.connect(process.env.MONGODB_CONNECTION)
+        const connection = mongoose.connect(process.env.MONGODB_CONNECTION_ATLAS)
 
         if (connection) {
             console.log('mongodb Connected Successfully👍');
